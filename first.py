a0c61ca0c8a5debc717fed2ab97284c3bbecec87
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-07-23 14:45:59 +0100

    Problems in python

--- first.py
+++ first.py
@@ -19,25 +19,39 @@
 
 # print(userAge(90))
 
-def studentScore(score):
-    grade = " "
-    if score >= 70 and score <=100:
-        grade = "A"
-    elif score <= 60:
-        grade = "B"
-    elif score <= 50:
-        grade = "C"
-    else:
-        grade = "F"
-    return grade
+# def studentScore(score):
+#     grade = " "
+#     if score >= 70 and score <=100:
+#         grade = "A"
+#     elif score <= 60:
+#         grade = "B"
+#     elif score <= 50:
+#         grade = "C"
+#     else:
+#         grade = "F"
+#     return grade
 
-stdGrade = studentScore(100)
+# stdGrade = studentScore(100)
 
-prefix = ""
-if stdGrade == "A":
-    prefix = "an"
+# prefix = ""
+# if stdGrade == "A":
+#     prefix = "an"
 
-print("You scored" + " " + prefix + " " + stdGrade)
+# print("You scored" + " " + prefix + " " + stdGrade)
 
 
 # Question: A town has a unique traffic rule based on your speed and weather. It is your birthday the fine varies
+def speedProgram(speed, isBirthday):
+    if speed <= 60:
+        print("No fine")
+    elif isBirthday == False and (speed > 60 or speed <= 80):
+        print("Small fine") 
+    elif isBirthday == True and (speed > 60 or speed <= 80):
+        print("No fine") 
+    elif isBirthday == False and (speed > 80):
+        print("Big fine")
+    elif isBirthday == True and (speed > 80):
+        print("Small fine")
+
+    
+print(speedProgram(71, True))
