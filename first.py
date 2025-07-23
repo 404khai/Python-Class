d4da715be19a85c49ed625c7580596c4f2d2576b
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-07-23 14:00:25 +0100

    Conditional statements, functions

--- first.py
+++ first.py
@@ -3,4 +3,18 @@
 
 # print(2==2)
 # print('mike'=='Mike')
-print(2!=1)
+#print(2!=1)
+
+# def add(a,b):
+#     return a + b
+# print(add(2, 3))
+
+# sum = add(45, 458)
+# print(f'sum' = sum)
+
+def userAge(age):
+    if age < 18:
+        return 'You are not eligible'
+    return 'Congratulations'
+
+print(userAge(90))
