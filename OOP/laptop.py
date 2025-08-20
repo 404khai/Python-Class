7a990a4b99459716bd945cc4efcac50d32d159e3
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-08-20 13:06:41 +0100

    Group laptop objects in List

--- OOP/laptop.py
+++ OOP/laptop.py
@@ -1,13 +1,13 @@
 class Laptops():
-    def __init__(self, make, model, color, year, serialNumber):
-        self.make = make
+    def __init__(self, brand, model, color, year, serialNumber):
+        self.brand = brand
         self.model = model
         self.color = color
         self.year = year
         self.serialNumber = serialNumber
 
     def __str__(self):
-        return f"{self.make}, {self.model}, {self.color}, {self.year}, {self.serialNumber}"
+        return f"{self.brand}, {self.model}, {self.color}, {self.year}, {self.serialNumber}"
     
 laptop1 = Laptops("Dell", "Alienware", "Black", 2003, "SN-111")
 laptop2 = Laptops("Dell", "XPS", "Green", 1993, "SN-141")
@@ -16,8 +16,8 @@ laptop4 = Laptops("HP", "Omen", "Silver", 2015, "SN-673")
 laptop5 = Laptops("Dell", "Inspiron", "Black", 2025, "SN-931")
 
 
-print(laptop1)
-print(laptop2)
-print(laptop3)
-print(laptop4)
-print(laptop5)
+laptopList = [laptop1, laptop2, laptop3, laptop4, laptop5]
+
+for lap in laptopList:
+    print(lap)
+
