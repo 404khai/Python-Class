82f9e22f028ab576f86d6bdc3596d419c315a8e0
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-08-20 12:29:35 +0100

    OOP methods & objects

--- OOP/info.py
+++ OOP/info.py
@@ -1,7 +1,22 @@
-class info():
-    greet = "How are you doing"
+#class info():
+#    greet = "How are you doing"
 
 #print(info)
-info1 = info()
+#info1 = info()
 
-print(info1.greet)
+#print(info1.greet)
+
+class Person():
+    def __init__(self, firstName, lastName):
+        self.firstName = firstName
+        self.lastName = lastName
+    def __str__(self):
+        return f"Fullname: {self.firstName} {self.lastName}"
+    
+    def user_details(self):
+        return f"Name: {self.firstName} {self.lastName}" 
+    
+p1 = Person("Daniels", "Fega")
+p1.lastName = "Prince"
+#print(p1)
+print(p1.user_details())
