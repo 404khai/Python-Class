bdcaaada0e67d20bfe16b2711ef198fee2523c6e
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-08-26 14:09:14 +0100

    Inheritance, Manager and Developer class called

---
+++ OOP/main.py
@@ -0,0 +1,11 @@
+from Manager import Manager
+from Developer import Developer
+
+empl = Manager("Nathan", 5000, "IT")
+emp2 = Developer("Prince", 2300, "Java")
+
+empl.showEmployeeDetails()
+print()
+print("______****______")
+print()
+emp2.showEmployeeDetails()
