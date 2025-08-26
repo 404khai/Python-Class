77619732625c112a0928165d40c7532709ede62c
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-08-26 15:05:02 +0100

    Encapsulation using Bank Class

--- OOP/main.py
+++ OOP/main.py
@@ -1,6 +1,7 @@
 from Manager import Manager
 from Developer import Developer
 from rest import Calculator
+from bank import BankAccount
 
 # empl = Manager("Nathan", 5000, "IT")
 # emp2 = Developer("Prince", 2300, "Java")
@@ -18,6 +19,8 @@ from rest import Calculator
 # print(calc.add(1, 3))
 # print(calc.add(9, 5, 6))
 
+
+#Polymorphism
 employees = [
     Manager("Mario", 4500, "HR"),
     Developer("Daniel", 7000, "Python")
@@ -27,3 +30,6 @@ for employee in employees:
     print(f"{employee.name}'s Bonus: {employee.calculateBonus()}")
 
 
+acc = BankAccount("Prince", 45000)
+acc.deposit(45000)
+print(acc.getBalance())
