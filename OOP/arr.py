13995fb99ffa9735e7ab7bbd18b0aaef5a74a2ce
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-01 13:06:18 +0100

    fruits, array manipulations

--- OOP/arr.py
+++ OOP/arr.py
@@ -16,11 +16,27 @@ import numpy as np
 # print(arr)
 # print(arr.ndim)
 
-#lst = [10,20,30,40,50]
+#list = [10,20,30,40,50]
 #print(lst)
 
 # arr = np.array([10,20,30])
 # print(arr[1])
 
+#arr = np.array([[1,2,3], [4,5,6], [30,40,50]])
+#print(arr[2,0])   #arrays are numbered in the order 0,1,2. picked by index array 0, index 2 is 3
+
+#print(arr.dtype)
+
+#fruits = np.array(["mango","apple","banana","orange"])
+#prin9t(fruits.dtype)
+
+#arr = np.array([1,2,3,4,5])
+#newArr = arr.astype('i')
+# print(arr)
+# print(newArr.dtype)
+
 arr = np.array([[1,2,3], [4,5,6], [30,40,50]])
-print(arr[2,0])   #arrays are numbered in the order 0,1,2. picked by index array 0, index 2 is 3
+print(arr.shape)
+print(arr.size)
+
+print(arr)
