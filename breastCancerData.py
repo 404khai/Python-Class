642d88bbe336c41824702e788a754bf53b0f3fc9
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-22 11:42:19 +0100

    Analyzing breast canser dataset

---
+++ breastCancerData.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = pd.read_csv("data.csv");
+
+# See last 5 rows
+print(data.head())
+
+# See last 5 rows
+print(data.tail())
+
+# import pandas as pd
+
+# data = pd.read_csv("wdbc.data");
+# print(data.tail())
