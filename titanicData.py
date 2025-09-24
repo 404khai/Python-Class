73c39c10fd2d55af0962369367f2cb3bb1b4677e
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-24 13:20:48 +0100

    more data manipulation using pandas

--- titanicData.py
+++ titanicData.py
@@ -12,3 +12,13 @@ df = pd.read_csv("titanic.csv");
 
 # df["Age"].fillna(df["Age"].median(), inplace=True)
 # print(df.describe)
+
+
+# df.drop("Cabin", axis=1, inplace=True)
+# df["Embarked"].fillna(df["Embarked"].mode()[0], inplace=True)
+
+# #drop irrelevant columns
+# df.drop(["PassengerId", "Name", "Ticket"], axis=1, inplace=True)
+# #mapping strings in a column of data to number values
+# df["Sex"] = df["Sex"].map({"male": 0, "female": 1})
+
