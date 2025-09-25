347eefa62d1a207aa63b0e95a917266a94efcfa2
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-25 08:27:31 +0100

    Visualizing data with seaborn sns

--- titanicData.py
+++ titanicData.py
@@ -12,9 +12,41 @@ df = pd.read_csv("titanic.csv")
 # print("\n Summary Statistics (numeric columns)\n", df.describe())
 
 #get missing values
-print("\n Missing values\n", df.isnull.sum())
+# print("\n Missing values\n", df.isnull().sum())
 
 
+#handle missing values
+# df["Age"].fillna(df["Age"].median(), inplace=True)
+# print("\n Missing values\n", df.isnull().sum())
+
+# df["Embarked"].fillna(df["Embarked"].mode()[0], inplace=True)
+# df.drop(columns="Cabin", inplace=True)
+
+# print("\n Value count for survived", df["Survived"].value_counts())
+# print("\n Gender count for people", df["Sex"].value_counts())
+
+
+# Visualize the distribution of survivors
+sns.countplot(x="Survived", data=df)
+plt.title("Survival Count (0 = Died | 1 = Survived)")
+plt.show()
+
+sns.countplot(x='Sex', hue='Survived', data=df)
+plt.title("Survival by Gender")
+plt.show()
+
+sns.histplot(df['Age'], bins=30, kde=True)
+plt.title("Age Distribution")
+plt.show()
+
+sns.boxplot(x='Pclass', y='Age', data=df)
+plt.title("Age Distribution by Passenger Class")
+plt.show()
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm', fmt=".2f")
+plt.title("Correlation Heatmap")
+plt.show()
 
 # See last 5 rows
 # print(df.head(3))
