c9ea03af717eaae38f4f0d0482ab6c22aafcae8c
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-10-18 21:52:05 +0100

    Sorting lists acending and descending order

--- jc.py
+++ jc.py
@@ -5,8 +5,22 @@ print(fruits[fruits.__len__() - 1])
 print(fruits[-1])
 print(type(fruits))
 
+#looping through the list
+#for fruit in fruits:
+#    print(fruit)
+
+#looping through the list using index
+#for i in range(len(fruits)):
+#    print(fruits[3])
+
 for fruit in fruits:
-    print(fruit)
+    if fruit == "cherry":
+        print("Found cherry!")
+        
+
+grades = [100, 50, 3, 1213, 65, 89]
+grades.sort(reverse=True)
+print(grades)
 # a list is ordered and changeable. Allows duplicate members
 # a tuple is ordered and unchangeable. Allows duplicate members
 # a set is unordered and unindexed. No duplicate members   
