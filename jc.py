2eba7af8c70398353d26c4bacae6e86af685ced6
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-10-18 22:36:17 +0100

    More learning

--- jc.py
+++ jc.py
@@ -21,6 +21,15 @@ for fruit in fruits:
 grades = [100, 50, 3, 1213, 65, 89]
 grades.sort(reverse=True)
 print(grades)
+
+#to make a duplicate copy of a list
+myGrades = grades.copy()
+myMathGrades = list(grades)
+
+print(myGrades)
+print(myMathGrades)
+
+ 
 # a list is ordered and changeable. Allows duplicate members
 # a tuple is ordered and unchangeable. Allows duplicate members
 # a set is unordered and unindexed. No duplicate members   
